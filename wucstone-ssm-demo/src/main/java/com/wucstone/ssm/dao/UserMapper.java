769395818039256f337a151e050b06b61fca9474
wuchuang <wuchuang@asiainfo.com>
2017-08-24 16:46:21 +0800

    mybatis demo 实例 加入log4j日志

---
+++ wucstone-ssm-demo/src/main/java/com/wucstone/ssm/dao/UserMapper.java
@@ -0,0 +1,8 @@
+package com.wucstone.ssm.dao;
+
+import java.util.Map;
+
+public interface UserMapper {
+
+	void insertUserInfo(Map userInfo);
+}
--- wucstone-ssm-demo/src/test/java/com/wucstone/ssm/TestMybatis.java
+++ wucstone-ssm-demo/src/test/java/com/wucstone/ssm/TestMybatis.java
@@ -1,5 +1,8 @@
 package com.wucstone.ssm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -11,11 +14,17 @@ public class TestMybatis {
 	@Test
 	public void MybatisTest(){
 		/** 先获得SqlSessionFactory 会话工厂对象 */
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Thread.class.getClassLoader().getResourceAsStream("mybatis-config.xml"));
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Thread.currentThread().getContextClassLoader().getResourceAsStream("mybatis-config.xml"));
 		/** 获得SqlSession 会话对象 */
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 		/** 使用会话工厂操作数据库 */
-		Student student = sqlSession.selectOne("com.xuyi.usemybatis.pojo.Student.selectStudentById", 4);
+		Map param=new HashMap();
+		param.put("userName","wucstone");
+		param.put("userAge",26);
+		param.put("userAddr","北京昌平天通苑");
+		sqlSession.insert("com.wucstone.ssm.dao.UserMapper.insertUserInfo", param);
+//		sqlSession.commit();
+		sqlSession.rollback();
 	}
 
 }

2d3cdd8bd57bbb8f1c3ba5c66163e5731c16cd6a
wuchuang <wuchuang@asiainfo.com>
2018-10-24 20:15:20 +0800

    feign客户端重构代码结构

---
+++ springCloud-parent/springCloud-api-member-service-impl/src/main/java/com/wucstone/service/impl/MemberServiceImpl.java
@@ -0,0 +1,21 @@
+package com.wucstone.service.impl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wucstone.entity.UserEntity;
+import com.wucstone.service.IMemberService;
+
+@RestController
+public class MemberServiceImpl implements IMemberService{
+
+	@RequestMapping("/getMember")
+	public UserEntity getMember(String name) {
+		
+		UserEntity entity = new UserEntity();
+		entity.setName(name);
+		entity.setAge(28);
+		return entity;
+	}
+
+}
